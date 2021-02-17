b37830f7fa0cfb98ad95b70f93d17dac07359810
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-02-17 13:57:02 -0500

    fixing prompt error

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -40,7 +40,7 @@ const saveNote = (note) =>
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
-  }).then(response => {
+  })/*.then(response => {
     if (response.ok) {
       return response.json();
     }
@@ -48,7 +48,7 @@ const saveNote = (note) =>
   }).then(postResponse => {
     console.log(postResponse);
     alert('Your note has been added.')
-  })
+  })*/
 
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
