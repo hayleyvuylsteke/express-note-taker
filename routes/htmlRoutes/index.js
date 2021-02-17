1ebeae57924460a885ac46a278cfa15f57e00b8e
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-02-17 13:53:19 -0500

    testing code - has 3 bugs

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 
 //html routes
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
   });
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
   });
 
 module.exports = router;
