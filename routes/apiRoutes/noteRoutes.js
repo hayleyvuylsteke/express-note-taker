e200a9d57ca32cec073fe71dc7cb0138e77dd6a1
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-02-20 10:04:10 -0500

    this code works

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -11,23 +11,22 @@ router.get('/notes', (req, res) => {
 })
 
 router.post('/notes', (req, res) => {
+    //Getting the length of the notes to be use as the next ID
+    let noteID = notes.length.toString();
 
-    //setting new note id
-    req.body.id = notes.length.toString();
+    //putting the body an ID to be the last element in the array
+    req.body.id = noteID;
 
     //run data through data validation
-    if (!validateNote(req.body)) {
+    if (validateNote(req.body)) {
         res.status(400).send('The note is not properly formatted');
+        return;
     } else {
-        const note = createNoteNote(req.body, notes);
-        res.json(note)
+        createNewNote(req.body, notes);
     }
-
-    //adding new note to json file and notes array
-    const note = createNewNote (req.body, notes)
-
     //console.log(req.body)
-    res.json(note)
+    //Returning the entire table
+    res.json(notes);
 });
 
 // delete functions
